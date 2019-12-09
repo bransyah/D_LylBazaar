0bf0282926e10fa9fb3e3945f471efc9c87b97cb
Sultan Bransyah <bransyah@users.noreply.github.com>
2019-12-10 00:06:10 +0700

    update

---
+++ src/app/penjualan/penjualan.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-penjualan',
+  templateUrl: './penjualan.page.html',
+  styleUrls: ['./penjualan.page.scss'],
+})
+export class PenjualanPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
